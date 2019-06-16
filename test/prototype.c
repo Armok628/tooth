642b51a7c46551b91c30c47fa8e48a65e0be3d56
Armok628 <Mark.Johnson.62899@gmail.com>
2019-06-15 20:41:34 -0500

    Fix prototype

--- test/prototype.c
+++ test/prototype.c
@@ -40,14 +40,13 @@ typedef struct link_s {
 enum { F_IMM=0x80, F_HID=0x40 };
 typedef void (*ffunc_t)(void);
 #define COUNT(...) sizeof (cell []){__VA_ARGS__}/sizeof(cell)
-#define LEN(x) (sizeof(x)/sizeof(x[0]))
 #define CWORD(last,name,cname) \
 void cname##_c(void); \
 struct { \
 	link_t link; \
 	cell xt[1]; \
 } cname = { \
-	{(link_t *)last,LEN(name)-1,name}, \
+	{(link_t *)last,name[0],name}, \
 	{(cell)cname##_c} \
 }; \
 void cname##_c(void)
@@ -56,7 +55,7 @@ struct { \
 	link_t link; \
 	cell xt[COUNT(__VA_ARGS__)]; \
 } cname = { \
-	{(link_t *)last,LEN(name)-1,name}, \
+	{(link_t *)last,name[0],name}, \
 	{__VA_ARGS__} \
 };
 /********************************/
@@ -99,30 +98,30 @@ void f_docol(void)
 	ip=&xt[1];
 	next();
 }
-CWORD(NULL,"EXIT",f_exit)
+CWORD(NULL,"\4EXIT",f_exit)
 {
 	ip=(ffunc_t *)rpop();
 	next();
 }
-CWORD(&f_exit.link,"LIT",f_lit)
+CWORD(&f_exit.link,"\3LIT",f_lit)
 {
 	push((cell)*ip);
 	ip=&ip[1];
 	next();
 }
-CWORD(&f_lit.link,"EXECUTE",f_execute)
+CWORD(&f_lit.link,"\7EXECUTE",f_execute)
 {
 	xt=(ffunc_t *)pop();
 	(*xt)();
 }
 /********************************/
-CWORD(&f_execute.link,"BRANCH",f_branch)
+CWORD(&f_execute.link,"\006BRANCH",f_branch)
 {
 	register cell o=(cell)*ip;
 	ip=(ffunc_t *)(&((char *)ip)[o]);
 	next();
 }
-CWORD(&f_branch.link,"0BRANCH",f_zbranch)
+CWORD(&f_branch.link,"\0070BRANCH",f_zbranch)
 {
 	if (!pop()) {
 		f_branch_c();
@@ -132,24 +131,24 @@ CWORD(&f_branch.link,"0BRANCH",f_zbranch)
 	next();
 }
 /********************************/
-CWORD(&f_zbranch.link,"DUP",f_dup)
+CWORD(&f_zbranch.link,"\003DUP",f_dup)
 {
 	push(sp[-1]);
 	next();
 }
-CWORD(&f_dup.link,"DROP",f_drop)
+CWORD(&f_dup.link,"\004DROP",f_drop)
 {
 	pop();
 	next();
 }
-CWORD(&f_drop.link,"SWAP",f_swap)
+CWORD(&f_drop.link,"\004SWAP",f_swap)
 {
 	register cell b=pop(),a=pop();
 	push(b);
 	push(a);
 	next();
 }
-CWORD(&f_swap.link,"ROT",f_rot)
+CWORD(&f_swap.link,"\003ROT",f_rot)
 {
 	register cell c=pop(),b=pop(),a=pop();
 	push(b);
@@ -157,7 +156,7 @@ CWORD(&f_swap.link,"ROT",f_rot)
 	push(a);
 	next();
 }
-CWORD(&f_rot.link,"-ROT",f_unrot)
+CWORD(&f_rot.link,"\004-ROT",f_unrot)
 {
 	register cell c=pop(),b=pop(),a=pop();
 	push(c);
@@ -165,18 +164,18 @@ CWORD(&f_rot.link,"-ROT",f_unrot)
 	push(b);
 	next();
 }
-CWORD(&f_unrot.link,"OVER",f_over)
+CWORD(&f_unrot.link,"\004OVER",f_over)
 {
 	push(sp[-2]);
 	next();
 }
-CWORD(&f_over.link,"NIP",f_nip)
+CWORD(&f_over.link,"\003NIP",f_nip)
 {
 	register cell a=pop();
 	sp[-1]=a;
 	next();
 }
-CWORD(&f_nip.link,"TUCK",f_tuck)
+CWORD(&f_nip.link,"\004TUCK",f_tuck)
 {
 	register cell b=pop(),a=pop();
 	push(b);
@@ -185,38 +184,38 @@ CWORD(&f_nip.link,"TUCK",f_tuck)
 	next();
 }
 /********************************/
-CWORD(&f_tuck.link,">R",f_to_r)
+CWORD(&f_tuck.link,"\002>R",f_to_r)
 {
 	rpush(pop());
 	next();
 }
-CWORD(&f_to_r.link,"R>",f_from_r)
+CWORD(&f_to_r.link,"\002R>",f_from_r)
 {
 	push(rpop());
 	next();
 }
-CWORD(&f_from_r.link,"R@",f_r_fetch)
+CWORD(&f_from_r.link,"\002R@",f_r_fetch)
 {
 	push(rp[-1]);
 	next();
 }
 /********************************/
-CWORD(&f_r_fetch.link,"SP@",f_sp_fetch)
+CWORD(&f_r_fetch.link,"\003SP@",f_sp_fetch)
 {
 	push((cell)sp);
 	next();
 }
-CWORD(&f_sp_fetch.link,"SP!",f_sp_store)
+CWORD(&f_sp_fetch.link,"\003SP!",f_sp_store)
 {
 	sp=(cell *)pop();
 	next();
 }
-CWORD(&f_sp_store.link,"RP@",f_rp_fetch)
+CWORD(&f_sp_store.link,"\003RP@",f_rp_fetch)
 {
 	push((cell)rp);
 	next();
 }
-CWORD(&f_rp_fetch.link,"RP!",f_rp_store)
+CWORD(&f_rp_fetch.link,"\003RP!",f_rp_store)
 {
 	rp=(cell *)pop();
 	next();
@@ -228,28 +227,28 @@ CWORD(&f_rp_fetch.link,"RP!",f_rp_store)
 	sp[-1] op##= b; \
 	next(); \
 }
-CWORD(&f_rp_store.link,"+",f_add) OP2(+)
-CWORD(&f_add.link,"-",f_sub) OP2(-)
-CWORD(&f_sub.link,"*",f_mul) OP2(*)
-CWORD(&f_mul.link,"AND",f_and) OP2(&)
-CWORD(&f_and.link,"OR",f_or) OP2(|)
-CWORD(&f_or.link,"XOR",f_xor) OP2(^)
-CWORD(&f_xor.link,"LSHIFT",f_shl) OP2(<<)
-CWORD(&f_shl.link,"RSHIFT",f_shr) OP2(>>)
-CWORD(&f_shr.link,"/MOD",f_divmod)
+CWORD(&f_rp_store.link,"\001+",f_add) OP2(+)
+CWORD(&f_add.link,"\001-",f_sub) OP2(-)
+CWORD(&f_sub.link,"\001*",f_mul) OP2(*)
+CWORD(&f_mul.link,"\003AND",f_and) OP2(&)
+CWORD(&f_and.link,"\002OR",f_or) OP2(|)
+CWORD(&f_or.link,"\003XOR",f_xor) OP2(^)
+CWORD(&f_xor.link,"\006LSHIFT",f_shl) OP2(<<)
+CWORD(&f_shl.link,"\006RSHIFT",f_shr) OP2(>>)
+CWORD(&f_shr.link,"\004/MOD",f_divmod)
 {
 	register cell b=sp[-2],a=sp[-1];
 	sp[-2]=a%b;
 	sp[-1]=a/b;
 	next();
 }
-CWORD(&f_divmod.link,"1+",f_incr)
+CWORD(&f_divmod.link,"\0021+",f_incr)
 { sp[-1]++; next(); }
-CWORD(&f_incr.link,"1-",f_decr)
+CWORD(&f_incr.link,"\0021-",f_decr)
 { sp[-1]--; next(); }
-CWORD(&f_decr.link,"NEGATE",f_negate)
+CWORD(&f_decr.link,"\006NEGATE",f_negate)
 { sp[-1]=-sp[-1]; next(); }
-CWORD(&f_negate.link,"INVERT",f_invert)
+CWORD(&f_negate.link,"\006INVERT",f_invert)
 { sp[-1]=~sp[-1]; next(); }
 /********************************/
 #define CMPOP(op) \
@@ -258,61 +257,61 @@ CWORD(&f_negate.link,"INVERT",f_invert)
 	sp[-1]=sp[-1] op b?~0:0; \
 	next(); \
 }
-CWORD(&f_invert.link,"=",f_eq) CMPOP(==)
-CWORD(&f_eq.link,"<",f_lt) CMPOP(<)
-CWORD(&f_lt.link,">",f_gt) CMPOP(>)
-CWORD(&f_gt.link,"<=",f_gte) CMPOP(<=)
-CWORD(&f_gte.link,">=",f_lte) CMPOP(>=)
-CWORD(&f_lte.link,"<>",f_neq) CMPOP(!=)
+CWORD(&f_invert.link,"\001=",f_eq) CMPOP(==)
+CWORD(&f_eq.link,"\001<",f_lt) CMPOP(<)
+CWORD(&f_lt.link,"\001>",f_gt) CMPOP(>)
+CWORD(&f_gt.link,"\002<=",f_gte) CMPOP(<=)
+CWORD(&f_gte.link,"\002>=",f_lte) CMPOP(>=)
+CWORD(&f_lte.link,"\002<>",f_neq) CMPOP(!=)
 #define UCMPOP(op) \
 { \
 	register ucell b=(ucell)pop(); \
 	sp[-1]=(ucell)sp[-1] op b?~0:0; \
 	next(); \
 }
-CWORD(&f_neq.link,"U<",f_ult) UCMPOP(<)
-CWORD(&f_ult.link,"U>",f_ugt) UCMPOP(>)
-CWORD(&f_ugt.link,"U<=",f_ugte) UCMPOP(<=)
-CWORD(&f_ugte.link,"U>=",f_ulte) UCMPOP(>=)
+CWORD(&f_neq.link,"\002U<",f_ult) UCMPOP(<)
+CWORD(&f_ult.link,"\002U>",f_ugt) UCMPOP(>)
+CWORD(&f_ugt.link,"\003U<=",f_ugte) UCMPOP(<=)
+CWORD(&f_ugte.link,"\003U>=",f_ulte) UCMPOP(>=)
 /********************************/
 #define CONSTANT(x) { push((cell)x); next(); }
-CWORD(&f_ulte.link,"S0",f_s0) CONSTANT(stack)
-CWORD(&f_s0.link,"R0",f_r0) CONSTANT(rstack)
-CWORD(&f_r0.link,"F_IMM",f_imm) CONSTANT(F_IMM)
-CWORD(&f_imm.link,"F_HID",f_hid) CONSTANT(F_HID)
+CWORD(&f_ulte.link,"\002S0",f_s0) CONSTANT(stack)
+CWORD(&f_s0.link,"\002R0",f_r0) CONSTANT(rstack)
+CWORD(&f_r0.link,"\005F_IMM",f_imm) CONSTANT(F_IMM)
+CWORD(&f_imm.link,"\005F_HID",f_hid) CONSTANT(F_HID)
 cell *here=NULL;
-CWORD(&f_hid.link,"HERE",f_here) CONSTANT(here)
-CWORD(&f_here.link,"CELL",f_cell) CONSTANT(sizeof(cell))
+CWORD(&f_hid.link,"\004HERE",f_here) CONSTANT(here)
+CWORD(&f_here.link,"\004CELL",f_cell) CONSTANT(sizeof(cell))
 cell base=10;
-CWORD(&f_cell.link,"BASE",f_base) CONSTANT(&base)
-CWORD(&f_base.link,"DOCOL",f_docol_ptr) CONSTANT(f_docol)
+CWORD(&f_cell.link,"\004BASE",f_base) CONSTANT(&base)
+CWORD(&f_base.link,"\005DOCOL",f_docol_ptr) CONSTANT(f_docol)
 cell sourceid=0;
-CWORD(&f_docol_ptr.link,"SOURCE-ID",f_sourceid) CONSTANT(sourceid)
+CWORD(&f_docol_ptr.link,"\011SOURCE-ID",f_sourceid) CONSTANT(sourceid)
 cell in=0;
-CWORD(&f_sourceid.link,">IN",f_in) CONSTANT(in)
+CWORD(&f_sourceid.link,"\003>IN",f_in) CONSTANT(in)
 link_t *latest;
-CWORD(&f_in.link,"LATEST",f_latest) CONSTANT(&latest)
+CWORD(&f_in.link,"\006LATEST",f_latest) CONSTANT(&latest)
 /********************************/
-CWORD(&f_latest.link,"ALLOT",f_allot)
+CWORD(&f_latest.link,"\005ALLOT",f_allot)
 {
 	register cell i=pop();
 	here=(cell *)(&((char *)here)[i]);
 	next();
 }
-CWORD(&f_allot.link,"!",f_store)
+CWORD(&f_allot.link,"\001!",f_store)
 {
 	register cell *d=(cell *)pop();
 	register cell s=pop();
 	*d=s;
 	next();
 }
-CWORD(&f_store.link,"@",f_fetch)
+CWORD(&f_store.link,"\001@",f_fetch)
 {
 	register cell *s=(cell *)pop();
 	push(*s);
 	next();
 }
-FORTHWORD(&f_fetch.link,",",f_comma,
+FORTHWORD(&f_fetch.link,"\001,",f_comma,
 	(cell)f_docol,
 	(cell)f_here.xt,
 	(cell)f_store.xt,
@@ -320,19 +319,19 @@ FORTHWORD(&f_fetch.link,",",f_comma,
 	(cell)f_allot.xt,
 	(cell)f_exit.xt
 )
-CWORD(&f_comma.link,"C!",f_cstore)
+CWORD(&f_comma.link,"\002C!",f_cstore)
 {
 	register char *d=(char *)pop();
 	*d=pop();
 	next();
 }
-CWORD(&f_cstore.link,"C@",f_cfetch)
+CWORD(&f_cstore.link,"\002C@",f_cfetch)
 {
 	register char *s=(char *)pop();
 	push(*s);
 	next();
 }
-FORTHWORD(&f_cfetch.link,"C,",f_ccomma,
+FORTHWORD(&f_cfetch.link,"\002C,",f_ccomma,
 	(cell)f_docol,
 	(cell)f_here.xt,
 	(cell)f_cstore.xt,
@@ -342,7 +341,7 @@ FORTHWORD(&f_cfetch.link,"C,",f_ccomma,
 	(cell)f_exit.xt
 )
 /********************************/
-CWORD(&f_ccomma.link,"EMIT",f_emit)
+CWORD(&f_ccomma.link,"\004EMIT",f_emit)
 {
 	static char c=0;
 	c=pop();
@@ -364,7 +363,7 @@ cell refill(void)
 	sourceid=0;
 	return l;
 }
-CWORD(&f_emit.link,"REFILL",f_refill)
+CWORD(&f_emit.link,"\006REFILL",f_refill)
 {
 	push(refill()?~0:0);
 	next();
@@ -376,7 +375,7 @@ char key(void)
 			_exit(0);
 	return inbuf[in++];
 }
-CWORD(&f_refill.link,"KEY",f_key)
+CWORD(&f_refill.link,"\003KEY",f_key)
 {
 	push((cell)key());
 	next();
@@ -394,12 +393,12 @@ char *word(void)
 	wordbuf[0]=i;
 	return wordbuf;
 }
-CWORD(&f_key.link,"WORD",f_word)
+CWORD(&f_key.link,"\004WORD",f_word)
 {
 	push((cell)word());
 	next();
 }
-FORTHWORD(&f_word.link,"COUNT",f_count,
+FORTHWORD(&f_word.link,"\005COUNT",f_count,
 	(cell)f_docol,
 	(cell)f_dup.xt,
 	(cell)f_incr.xt,
@@ -408,7 +407,7 @@ FORTHWORD(&f_word.link,"COUNT",f_count,
 	(cell)f_exit.xt
 )
 /********************************/
-CWORD(&f_count.link,">NUMBER",f_tonumber)
+CWORD(&f_count.link,"\007>NUMBER",f_tonumber)
 {
 	register cell l=pop();
 	register char *s=(char *)pop();
@@ -454,15 +453,16 @@ link_t *find(char *cs)
 			continue;
 		if ((l->flags&~F_IMM)!=len)
 			continue;
+		char *ln=l->name+1;
 		for (int i=0;i<len;i++)
-			if (l->name[i]!=cs[i])
+			if (ln[i]!=cs[i])
 				goto NEXT;
 		break;
 NEXT:		continue;
 	}
 	return l;
 }
-CWORD(&f_tonumber.link,"FIND",f_find)
+CWORD(&f_tonumber.link,"\004FIND",f_find)
 {
 	char *s=(char *)pop();
 	link_t *l=find(s);
@@ -475,14 +475,14 @@ CWORD(&f_tonumber.link,"FIND",f_find)
 	}
 	next();
 }
-CWORD(&f_find.link,"'",f_tick)
+CWORD(&f_find.link,"\001'",f_tick)
 {
 	link_t *l=find(word());
 	push((cell)(&((ffunc_t *)l)[3]));
 	next();
 }
 /********************************/
-FORTHWORD(&f_tick.link,"ALIGNED",f_aligned,
+FORTHWORD(&f_tick.link,"\007ALIGNED",f_aligned,
 	(cell)f_docol,
 	(cell)f_decr.xt,
 	(cell)f_cell.xt,
@@ -493,7 +493,7 @@ FORTHWORD(&f_tick.link,"ALIGNED",f_aligned,
 	(cell)f_add.xt,
 	(cell)f_exit.xt
 )
-FORTHWORD(&f_aligned.link,"ALIGN",f_align,
+FORTHWORD(&f_aligned.link,"\005ALIGN",f_align,
 	(cell)f_docol,
 	(cell)f_here.xt,
 	(cell)f_dup.xt,
@@ -505,7 +505,7 @@ FORTHWORD(&f_aligned.link,"ALIGN",f_align,
 )
 link_t *latest=(link_t *)&f_align.link;
 /********************************/
-FORTHWORD(NULL,"",baseinterp,
+FORTHWORD(NULL,"\00",baseinterp,
 	(cell)f_docol,
 	(cell)f_word.xt,
 	(cell)f_find.xt,
