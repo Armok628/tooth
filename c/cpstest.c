474d8e7832aa6694e0ed4a81dad8ae80bdaf5183
Armok628 <Mark.Johnson.62899@gmail.com>
2019-06-18 21:07:46 -0500

    More continuation passing testing

--- c/cpstest.c
+++ c/cpstest.c
@@ -1,46 +1,65 @@
-#include <stdio.h>
-#include <stdint.h>
 #include <unistd.h>
-#if INTPTR_MAX > SIZE_MAX
-#error "Incompatible type sizes"
+#include <stdint.h>
+#if INTPTR_MAX > 0xFFFFFFFF
+typedef int64_t cell;
+#elif INTPTR_MAX > 0xFFFF
+typedef int32_t cell;
+#else
+typedef int16_t cell;
 #endif
-typedef size_t cell;
 /******** VM instructions ********/
-typedef void (*func_t)();
+typedef void (*func)();
 #define push(sp,val) (*(++sp)=(cell)(val))
 #define pop(sp) (*(sp--))
-inline void next(func_t *ip,cell *sp,cell *rp)
+void next(func *ip,cell *sp,cell *rp)
 {
 	(*ip)(ip+1,sp,rp);
 }
-void f_exit(func_t *ip,cell *sp,cell *rp)
+void f_docol(func *ip,cell *sp,cell *rp)
 {
-	ip=(func_t *)pop(rp);
+	push(rp,ip+1);
+	ip=(func *)*ip;
 	next(ip,sp,rp);
 }
-void f_lit(func_t *ip,cell *sp,cell *rp)
+void f_exit(func *ip,cell *sp,cell *rp)
 {
-	push(sp,*(ip++));
+	ip=(func *)pop(rp);
 	next(ip,sp,rp);
 }
-void f_bye(func_t *ip,cell *sp,cell *rp)
+void f_lit(func *ip,cell *sp,cell *rp)
 {
-	register cell a=pop(sp);
-	_exit(a);
+	push(sp,*ip);
+	next(ip+1,sp,rp);
+}
+void f_bye(func *ip,cell *sp,cell *rp)
+{
+	_exit(pop(sp));
 	next(ip,sp,rp);
 }
-void f_add(func_t *ip,cell *sp,cell *rp)
+#define F_2OP(name,op) \
+void name(func *ip,cell *sp,cell *rp) \
+{ \
+	register cell b=pop(sp),a=pop(sp); \
+	push(sp,a op b); \
+	next(ip,sp,rp); \
+}
+F_2OP(f_add,+)
+F_2OP(f_sub,-)
+F_2OP(f_mul,*)
+void f_divmod(func *ip,cell *sp,cell *rp)
 {
-	register cell a=pop(sp),b=pop(sp);
-	push(sp,a+b);
+	register cell b=pop(sp),a=pop(sp);
+	push(sp,a%b);
+	push(sp,a/b);
 	next(ip,sp,rp);
 }
 /******** Entry ********/
-#define LIT(n) f_lit,(func_t)n
-func_t prog[]={LIT(1),LIT(2),f_add,f_bye};
+#define LIT(n) f_lit,(func)n
+func f_oneplus[]={LIT(1),f_add,f_exit};
+func prog[]={LIT(2),f_docol,(func)f_oneplus,f_bye};
 cell stack[1024];
 cell rstack[1024];
-int main()//(int argc,char *argv[])
+int main()/*(int argc,char *argv[])*/
 {
 	next(prog,stack,rstack);
 	return 0;
