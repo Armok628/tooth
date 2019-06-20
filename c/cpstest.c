b44e29af55de57d04e775e3e9fc113b979f87fcc
Armok628 <Mark.Johnson.62899@gmail.com>
2019-06-19 21:32:07 -0500

    FORTHWORD macro for new C prototype

--- c/cpstest.c
+++ c/cpstest.c
@@ -23,9 +23,10 @@ typedef struct link_s {
 /*
  * 		CWORD macro
  * Used to define words in C visible to the system.
+ * Name is a counted string literal, e.g. "\004WORD"
  * Creates two definitions from parameter cname:
  * (1) f_cname: the link structure for the word.
- * 	(execution token is available as f_cname.xt)
+ * 	(execution token at f_cname.xt, decays to (func_t *))
  * (2) f_cname_c: the C function which implements the word.
  * 	(can be aliased to func_t)
  */
@@ -40,6 +41,24 @@ struct { \
 	{f_##cname##_c,f_exit_c} \
 }; \
 void f_##cname##_c(func_t *ip,cell_t *sp,cell_t *rp)
+
+/*
+ *		FORTHWORD/ENDWORD macros
+ * Used to define assembler-compiled Forth words; same arguments as CWORD.
+ * Parameter deflen represents number of given cells, not counting f_exit_c.
+ * Defines a struct f_cname whose execution token is f_cname.xt.
+ * Usage e.g.: FORTHWORD(...) {f_lit_c,(func_t)1,f_add_c,f_exit_c} ENDWORD
+ */
+
+#define FORTHWORD(last,len,name,cname,deflen) \
+struct { \
+	link_t link; \
+	func_t xt[deflen]; \
+} f_##cname = { \
+	{last,len,name},
+
+#define ENDWORD };
+
 
 /*________ VM instructions ________*/
 
@@ -88,10 +107,10 @@ CWORD(NULL,4,"\004EXIT",exit)
  * N.B.: This behavior is different from normal ITC compilers
  */
 
-CWORD(NULL,5,"\005DOCOL",docol)
+CWORD(&f_exit.link,5,"\005DOCOL",docol)
 {
 	push(rp,ip+1);
-	ip=(func_t *)*ip;
+	ip=*(func_t **)ip;
 	next(ip,sp,rp);
 }
 
@@ -100,7 +119,7 @@ CWORD(NULL,5,"\005DOCOL",docol)
  * f_lit_c will place the cell after it in the code field onto the stack.
  */
 
-CWORD(NULL,3,"\004LIT",lit)
+CWORD(&f_docol.link,3,"\004LIT",lit)
 {
 	push(sp,*ip);
 	next(ip+1,sp,rp);
@@ -110,7 +129,7 @@ CWORD(NULL,3,"\004LIT",lit)
  *		bye
  * For testing purposes, bye does not currently exhibit typical behavior.
  */
-CWORD(NULL,3,"\004BYE",bye)
+CWORD(&f_lit.link,3,"\004BYE",bye)
 {
 	_exit(pop(sp)); /*_exit(0);*/
 	next(ip,sp,rp);
@@ -121,7 +140,7 @@ CWORD(NULL,3,"\004BYE",bye)
  * f_execute_c executes the code field pointer on top of the stack.
  * Because all code fields end with f_exit_c, execute can cope with any XT.
  */
-CWORD(NULL,7,"\007EXECUTE",execute)
+CWORD(&f_bye.link,7,"\007EXECUTE",execute)
 {
 	push(rp,ip);
 	ip=(func_t *)pop(sp);
@@ -130,13 +149,13 @@ CWORD(NULL,7,"\007EXECUTE",execute)
 
 /*________ Branching ________*/
 
-CWORD(NULL,6,"\006BRANCH",branch)
+CWORD(&f_execute.link,6,"\006BRANCH",branch)
 {
 	ip+=(cell_t)*ip;
 	next(ip,sp,rp);
 }
 
-CWORD(NULL,7,"\0070BRANCH",zbranch)
+CWORD(&f_branch.link,7,"\0070BRANCH",zbranch)
 {
 	ip+=pop(sp)?(cell_t)*ip:1;
 	next(ip,sp,rp);
@@ -150,13 +169,23 @@ CWORD(NULL,7,"\0070BRANCH",zbranch)
 	push(sp,a op b); \
 	next(ip,sp,rp); \
 }
-CWORD(NULL,1,"\001+",add)
+CWORD(&f_zbranch.link,1,"\001+",add)
 	F_2OP(+)
-CWORD(NULL,1,"\001-",sub)
+CWORD(&f_add.link,1,"\001-",sub)
 	F_2OP(-)
-CWORD(NULL,1,"\001*",mul)
+CWORD(&f_sub.link,1,"\001*",mul)
 	F_2OP(*)
-CWORD(NULL,4,"\004/MOD",divmod)
+CWORD(&f_sub.link,3,"\001AND",and)
+	F_2OP(&)
+CWORD(&f_sub.link,2,"\001OR",or)
+	F_2OP(|)
+CWORD(&f_sub.link,3,"\001XOR",xor)
+	F_2OP(^)
+CWORD(&f_mul.link,6,"\006LSHIFT",lshift)
+	F_2OP(<<)
+CWORD(&f_lshift.link,6,"\006RSHIFT",rshift)
+	F_2OP(>>)
+CWORD(&f_rshift.link,4,"\004/MOD",divmod)
 {
 	register cell_t b=pop(sp),a=pop(sp);
 	push(sp,a%b);
@@ -164,15 +193,35 @@ CWORD(NULL,4,"\004/MOD",divmod)
 	next(ip,sp,rp);
 }
 
+/*________ Comparisons  ________*/
+
+#define F_CMP(op) \
+{ \
+	register cell_t b=pop(sp),a=pop(sp); \
+	push(sp,a op b?~0:0); \
+	next(ip,sp,rp); \
+}
+
+CWORD(&f_divmod.link,1,"\001=",eq)
+	F_CMP(==)
+
 /*________ Entry ________*/
 
-#define LIT(n) f_lit_c,(func_t)n
-func_t f_oneplus[]={LIT(1),f_add_c,f_exit_c};
-func_t prog[]={LIT(1),LIT(2),LIT(f_add.xt),f_execute_c,f_bye_c};
+#define F(x) (func_t)(cell_t)x
+/* ^ Double typecast to ignore warnings from code field pointers */
+#define P(n) f_##n##_c /* P(rimitive) */
+#define NP(n) P(docol),F(f_##n.xt) /* N(on)P(rimitive) */
+#define LT(x) P(lit),F(x) /* L(i)T(eral) */
+FORTHWORD(NULL,2,"\0021+",oneplus,4) {
+	LT(1),P(add),P(exit)
+} ENDWORD
+FORTHWORD(NULL,0,"\000",prog,5) {
+	LT(2),NP(oneplus),P(bye)
+} ENDWORD
 cell_t stack[1024];
 cell_t rstack[1024];
 int main()/*(int argc,char *argv[])*/
 {
-	next(prog,&stack[1024],&rstack[1024]);
+	next(f_prog.xt,&stack[1024],&rstack[1024]);
 	return 0;
 }
